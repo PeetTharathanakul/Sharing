1a52b2721cc5ff0b906957c830160400af9521be
Peet <peet12131424@gmail.com>
2023-01-20 18:00:07 +0700

    -

--- Assets/Scripts/BattlePass/Controller/ItemGroup.cs
+++ Assets/Scripts/BattlePass/Controller/ItemGroup.cs
@@ -1,18 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ItemGroup : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Itemlist list;
+    public Text LvText;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
