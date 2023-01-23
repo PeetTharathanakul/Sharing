ff5522a798c135a5fdf005da74b49681586b9228
peet_dev <peet12131424@gmail.com>
2023-01-23 16:49:55 +0700

    -

--- Assets/Scripts/BattlePass/Scriptable/Items.cs
+++ Assets/Scripts/BattlePass/Scriptable/Items.cs
@@ -3,16 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "New Item", menuName = "BattlePass/Items")]
-public class Items : ScriptableObject
-{
-    public ItemDetails[] Item;
-}
-
 [System.Serializable]
-public class ItemDetails
+public class Items : ScriptableObject
 {
     public string Name;
     public Sprite ItemSprite;
     public string Description;
     public int Value;
 }
+
