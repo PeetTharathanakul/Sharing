0a803aa4806055e4470dbf202eeb120cb1a7caa5
Peet <peet12131424@gmail.com>
2023-06-01 10:38:54 +0700

    bruh

--- Assets/Project/Scripts/Skill/Skillcutin.cs
+++ Assets/Project/Scripts/Skill/Skillcutin.cs
@@ -1,8 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Skillcutin : MonoBehaviour
 {
-    public List<Animation> a;
+    public GameObject Cutprefab;
+    public List<Animation> Cutinanimation;
+    public List<Sprite> Shapesprite;
+    public Image Cutinimg;
+    public ParticleSystem Cutinparticle;
+
+    public void Cutin(int index)
+    {
+        Cutinimg.sprite = Shapesprite[index];
+        //Cutinparticle.shape.shapeType. = Shapesprite[index];
+    }
+
 }
